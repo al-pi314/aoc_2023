70528efd863a32224b558776607ff5c096ccea61
al-pi314 <aleksander.piciga@gmail.com>
2023-12-15 23:40:29 +0100

    day 8 part 2 by LCM

--- 08/code.py
+++ 08/code.py
@@ -1,34 +1,46 @@
-from collections import deque
+from functools import reduce
+from math import lcm
 
 with open("input.txt", "r") as file:
+    # Part 1
+    # start = 'AAA'
+    # end = 'ZZZ'
+    
+    # Part 2
+    start = 'A'
+    end = 'Z'
+
+    # Read Directions
     direction = list(file.readline().strip()) 
     file.readline()
 
-    start = 'A'
-    end = 'Z'
-    origins = []
-
+    # Read Graph Structure
     graph = {}
+    current = []
     for line in file.readlines():
         data = line.split(" = ")
         node = data[0]
-
-        connections = data[1].replace("(", "").replace(")", "").strip().split(", ")
-        graph[node] = connections
-
+        graph[node] = data[1].replace("(", "").replace(")", "").strip().split(", ")
         if node.endswith(start):
-            origins.append(node)
-
-    def loop(node, i, visited):
-        rel_i = i % len(direction)
-        k = (node, rel_i)
-        print(k)
-        if k in visited:
-            print("loop", visited[k])
-            return
-        visited[k] = i
-        
-        d = 0 if direction[rel_i] == 'L' else 1
-        loop(graph[node][d], i + 1, visited)
+            current.append(node)
     
-    loop('ZZZ', 0, {})
+    # Find the first cycle
+    # It is not stated in the instructions but there always will be a cycle that ends on an end node
+    # Than we can just use LCM of all cycle lengths to calculate when they will all reach the end
+    # But this should not be the general solution as per problem statment
+    visited = [{} for _ in current]
+    cycle_length = [-1 for _ in current]
+    step = 0
+    while any([l == -1 for l in cycle_length]):
+        for i in range(len(current)):
+            if cycle_length[i] != -1:
+                continue
+            rel_step = step % len(direction)
+            k = (current[i], rel_step)
+            if k in visited[i]:
+                cycle_length[i] = len(visited[i]) - visited[i][k]
+                continue
+            visited[i][k] = rel_step
+            current[i] = graph[current[i]][direction[rel_step] == 'R']
+        step += 1
+    print(reduce(lcm, cycle_length))
